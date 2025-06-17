5ac5101cdad7646795610e0031f93a19225d4f1c
Abhinav Pandey <aebhynaw@gmail.com>
2025-06-17 23:19:19 +0530

    Create task_1.py

---
+++ task_1.py
@@ -0,0 +1,10 @@
+# perform basic mathematical operations
+
+num_1 = int(input("Enter the first number: "))
+num_2 = int(input("Enter the second number: "))
+
+print("\n")
+print("Addition:", num_1 + num_2)
+print("Subtraction:", num_1 - num_2)
+print("Multiplication:", num_1 * num_2)
+print("Division:", num_1 / num_2)
